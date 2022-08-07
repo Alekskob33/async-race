3166278eecaecb606dd7c76c7f6246dae97bdf08
alekskob33 <alekskob33@yandex.ru>
2022-08-08 01:46:08 +0300

    fix: add condition for getUrl

--- async-race/src/js/utilities.ts
+++ async-race/src/js/utilities.ts
@@ -1,9 +1,12 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 export const getUrl = <T>(dir: string, queryParams: T): string => {
   const urlBase = 'http://127.0.0.1:3000';
-  const queryString = Object.entries(queryParams).reduce((str, pair) => {
-    return `${str}${pair[0]}=${pair[1]}&`;
-  }, '');
-  const url = `${urlBase}${dir}?${queryString}`;
-  return url;
+  if (queryParams) {
+    const queryString = Object.entries(queryParams).reduce((str, pair) => {
+      return `${str}${pair[0]}=${pair[1]}&`;
+    }, '');
+    const url = `${urlBase}${dir}?${queryString}`;
+    return url;
+  }
+  return `${urlBase}${dir}`;
 };
