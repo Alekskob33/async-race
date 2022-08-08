889c8006dbddbacf8332da35ec0356871e9c3f12
alekskob33 <alekskob33@yandex.ru>
2022-08-08 21:14:10 +0300

    feat: add test instances in index.ts

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,1 +1,21 @@
 import './style.sass';
+
+import Car from './js/car/car';
+import Garage from './js/garage/garage';
+
+const Renault = new Car('Logan', 'gray');
+
+// Renault.startEngine({ id: 1 });
+// Renault.stopEngine({ id: 3 });
+// Renault.selectDriveMode({ id: 1 });
+// Renault.go({ id: 2 });
+
+const garage = new Garage();
+
+// garage.getCars();
+// garage.getCars({ _page: 1, _limit: 2 });
+
+// garage.createCar({ name: 'Moskwich', color: 'red' });
+// garage.getCar({ id: 12 });
+// garage.deleteCar({ id: 4 });
+// garage.updateCar({ id: 3, name: 'Mitsubishi', color: 'red' });
