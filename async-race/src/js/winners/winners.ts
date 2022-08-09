637d7d3ef370dd1ea7daecccf689eb9da460947c
alekskob33 <alekskob33@yandex.ru>
2022-08-09 14:56:27 +0300

    feat: complite Winners Class

--- async-race/src/js/winners/winners.ts
+++ async-race/src/js/winners/winners.ts
@@ -19,6 +19,74 @@ class Winners implements TWinners {
       console.log(err);
     }
   }
+
+  async getWinner({ id }: { id: number }) {
+    const url = getUrl(`/winners/${id}`, {});
+
+    try {
+      const response = await fetch(url);
+      if (!response.ok) throw 'response was not ok';
+      const body = (await response.json()) as winner;
+      console.log(body);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async createWinner({ id, wins, time }: winner) {
+    const url = getUrl('/winners', {});
+
+    try {
+      const response = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ id, wins, time }),
+      });
+      if (!response.ok) {
+        const message = await response.text();
+        throw message;
+      } else {
+        const body = (await response.json()) as winner;
+        console.log(body);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async deleteWinner({ id }: { id: number }) {
+    const url = getUrl(`/winners/${id}`, {});
+
+    try {
+      const response = await fetch(url, { method: 'DELETE' });
+      if (!response.ok) throw 'response was not ok';
+      const body = (await response.json()) as {};
+      console.log(`The winner id=${id} was delete. ${body}`);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  async updateWinner({ id, wins, time }: winner) {
+    const url = getUrl(`/winners/${id}`, {});
+
+    try {
+      const response = await fetch(url, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ wins, time }),
+      });
+      if (!response.ok) {
+        const message = await response.json() as {};
+        throw message;
+      } else {
+        const body = (await response.json()) as winner;
+        console.log(body);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
 }
 
 export default Winners;
